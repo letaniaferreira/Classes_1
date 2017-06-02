6bd2e3f5edad79bf0efbae27e3d1a7fbb21b1c96
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-01 17:29:38 -0700

    Unified domestic and international with a parent class

---
+++ melons.py
@@ -0,0 +1,49 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder(object):
+    """Both domestic and international melon orders"""
+
+    def __init__(self, species, qty, order_type, tax):
+        """All melon order attributes"""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax 
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    def __init__(self,species,qty):
+        """Initialize melon order attributes."""
+        super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08)
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
+
+        self.country_code = country_code
+    
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
