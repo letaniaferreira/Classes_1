9c1cc6067a45d84f8096444cc9bea703d198ef90
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-01 17:55:23 -0700

    Started working on Christmas Melon order

--- melons.py
+++ melons.py
@@ -47,3 +47,25 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class ChristmasMelonOrder(AbstractMelonOrder):
+    """Christmas Melon Order"""
+
+    def __init__(self, species, qty, country_code):
+        """INitialize melon order attributes"""
+        super(ChristmasMelonOrder, self). __init__(species, qty, christmas_price)
+
+        self.country_code = country_code
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5 * 1.5
+        if qty >= 10:
+            total = (1 + self.tax) * self.qty * base_price
+            if order_type == "international" and qty < 10:
+                total = ((1 + self.tax) * self.qty * base_price) + 3
+            else:
+                total = (1 + self.tax) * self.qty * base_price
+
+        return total
